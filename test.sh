ef7276976060052f2c814002a51335c0cb152466
lorenuars19 <lorenuar.s19@gmail.com>
2022-05-19 17:34:29 +0200

    -

--- test.sh
+++ test.sh
@@ -71,16 +71,18 @@ ANIM=(
 "
 )
 
-for u in {0..3}; do
+for u in {1..2}; do
 	for ((i = 0; i < ${#ANIM[@]}; i++)); do
 		printf "\e[36;1m%s\e[5A\e[0G\e[0m" ${ANIM[i]}
 		sleep 0.025
 	done
 done
+printf "\n\n\n\n\n\n"
 
 git add . ; git commit -am "-"; git push
 
-for u in {0..3}; do
+
+for u in {1..2}; do
 	for ((i = 0; i < ${#ANIM[@]}; i++)); do
 		printf "\e[36;1m%s\e[5A\e[0G\e[0m" ${ANIM[i]}
 		sleep 0.025
