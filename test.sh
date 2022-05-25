c92a591d258cd1363ed73f14a2cae7f9d690db52
lorenuars19 <lorenuar.s19@gmail.com>
2022-05-25 12:57:56 +0200

    TAMER

--- test.sh
+++ test.sh
@@ -71,20 +71,21 @@ ANIM=(
 "
 )
 
-sleep 10 &
+git add . ; git commit -am "TAMER"; git push -q origin master&
 PID=$$
 
 echo PID : $PID
 
-
-git add . ; git commit -qam "-"; git push -q &
 
 for u in {1..5}; do
 	for ((i = 0; i < ${#ANIM[@]}; i++)); do
 		printf "\e[36;1m%s\e[5A\e[0G\e[0m" ${ANIM[i]}
 		sleep 0.025
 
-		
+		if [[ -z $(ps | grep $PID) ]]
+		then
+			exit
+		fi
 
 
 	done
