2aed53a4143ad441d4d1966ffd1403a6b74b19a5
lorenuars19 <lorenuar.s19@gmail.com>
2022-05-25 15:26:09 +0200

    TAMER

--- oneline.sh
+++ oneline.sh
@@ -1,11 +1,6 @@
-SHELL=/bin/bash
+set -vx
 
-all : save
-
-sauce : save
-
-save :
-	@-git add . ; git commit -am "TAMER"; git push &\
+git add . ; git commit -am "TAMER"; git push & \
 	CPID=$$! ;\
 	IFS=""; \
 	ANIM=( \
@@ -61,19 +56,19 @@ save :
 	\
 	"| ||     |"\
 	"| ||     |"\
-	"| \\/     |" \
+	"| \\/     |"\
 	"| SAVING |"\
-	" \
-	)\
-	while true; do \
-	for ((i = 0; i < ${#ANIM[@]}; i = i )); do \
-		printf "\e[36;1m" \
-			for ((j = 0; j < 4; j++)); do \
-				printf "%s\n" ${ANIM[i]} \
-				let "i++" \
-			done \
-		printf "\e[4A\e[0G\e[0m" \
-		sleep 0.040 \
-		if [[ -z $(ps -o pid | grep "$PID") ]]; then exit; fi \
-		done \
-	done \
+	);\
+	while true \
+	;do \
+		for ((i=0;i<${#ANIM[@]};i=i)) \
+		;do \
+			printf "\e[36;1m"\
+				;for ((j=0;j<4;j++))\
+				;do \
+					printf "%s\n" ${ANIM[i]} ; let "i++" \
+				;done ;\
+			printf "\e[4A\e[0G\e[0m" ; sleep 0.040 \
+			;if [[ -z $(ps -o pid | grep "$CPID") ]];then exit ; fi \
+		;done \
+	;done
